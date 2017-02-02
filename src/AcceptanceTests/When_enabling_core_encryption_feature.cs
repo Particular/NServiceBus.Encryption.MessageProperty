fbca52be1813fe342b12c5b91cd15694e8a66476
Tim Bussmann <timbussmann@users.noreply.github.com>
2017-02-02 16:33:52 +0100

    Check for redundant encryption configuration (#5)
    * check for redundant encryption configuration
    
    * revert EncryptedServiceConstructorKey change

---
+++ src/AcceptanceTests/When_enabling_core_encryption_feature.cs
@@ -0,0 +1,44 @@
+﻿namespace NServiceBus.Encryption.MessageProperty.AcceptanceTests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using AcceptanceTesting;
+    using NUnit.Framework;
+
+
+    [TestFixture]
+    public class When_enabling_core_encryption_feature : NServiceBusAcceptanceTest
+    {
+        [Test]
+        public void Should_throw_at_startup()
+        {
+            var exception = Assert.ThrowsAsync<Exception>(() =>
+                Scenario.Define<ScenarioContext>()
+                    .WithEndpoint<EncryptionEndpoint>()
+                    .Done(c => c.EndpointsStarted)
+                    .Run());
+
+            StringAssert.Contains("The message property encryption extension as well as NServiceBus.Core's encryption feature are enabled. Disable one of the encryption features to avoid message payload corruption.", exception.Message);
+        }
+
+        class EncryptionEndpoint : EndpointConfigurationBuilder
+        {
+            public EncryptionEndpoint()
+            {
+                EndpointSetup<DefaultServer>(c =>
+                {
+                    // ReSharper disable once InvokeAsExtensionMethod
+                    ConfigureRijndaelEncryptionService.RijndaelEncryptionService(c, "keyIdentifier1", new Dictionary<string, byte[]>()
+                    {
+                        {"keyIdentifier1", Encoding.ASCII.GetBytes("aaaaaaaaaabbbbbbbbbbcccc")}
+                    });
+                    NServiceBus.ConfigureRijndaelEncryptionService.RijndaelEncryptionService(c, "keyIdentifier2", new Dictionary<string, byte[]>
+                    {
+                        {"keyIdentifier2", Encoding.ASCII.GetBytes("ddddddddddeeeeeeeeeeffff")}
+                    });
+                });
+            }
+        }
+    }
+}
--- src/MessageProperty/Encryption.cs
+++ src/MessageProperty/Encryption.cs
@@ -1,5 +1,6 @@
 ﻿namespace NServiceBus.Encryption.MessageProperty
 {
+    using System;
     using Features;
 
     class Encryption : Feature
@@ -13,6 +14,13 @@
 
         protected override void Setup(FeatureConfigurationContext context)
         {
+            // check if the encryption service in the core has been enabled.
+            // this check can be removed when encryption has been fully obsoleted in the core.
+            if (context.Settings.HasSetting("EncryptionServiceConstructor"))
+            {
+                throw new Exception("The message property encryption extension as well as NServiceBus.Core's encryption feature are enabled. Disable one of the encryption features to avoid message payload corruption.");
+            }
+
             var serviceConstructor = context.Settings.GetEncryptionServiceConstructor();
             var service = serviceConstructor();
             var inspector = new EncryptionInspector(context.Settings.Get<Conventions>());
