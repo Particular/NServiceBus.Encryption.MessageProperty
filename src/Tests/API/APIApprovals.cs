bc4675ac8ee846c7e75116734ad088c39efea4cd
Sean Farmar <s.farmar@gmail.com>
2019-02-14 15:35:04 +0000

    support10-update testing packages (#34)
    * Update testing packages
    
    * Update NuGetPackager to 0.6.5
    
    * Update Particular.CodeRules to 0.2.1
    
    * Switch to Particular.Approvals

--- src/Tests/API/APIApprovals.cs
+++ src/Tests/API/APIApprovals.cs
@@ -1,32 +1,17 @@
 ﻿namespace NServiceBus.Encryption.MessageProperty.Tests
 {
-    using System;
-    using System.Linq;
-    using System.Runtime.CompilerServices;
-    using ApprovalTests;
     using NUnit.Framework;
+    using Particular.Approvals;
     using PublicApiGenerator;
 
     [TestFixture]
     public class APIApprovals
     {
         [Test]
-        [MethodImpl(MethodImplOptions.NoInlining)]
         public void ApproveNServiceBus()
         {
-            var publicApi = Filter(ApiGenerator.GeneratePublicApi(typeof(RijndaelEncryptionService).Assembly));
-            Approvals.Verify(publicApi);
-        }
-
-        string Filter(string text)
-        {
-            return string.Join(Environment.NewLine, text.Split(new[]
-            {
-                Environment.NewLine
-            }, StringSplitOptions.RemoveEmptyEntries)
-                .Where(l => !l.StartsWith("[assembly: ReleaseDateAttribute("))
-                .Where(l => !string.IsNullOrWhiteSpace(l))
-                );
+            var publicApi = ApiGenerator.GeneratePublicApi(typeof(RijndaelEncryptionService).Assembly);
+            Approver.Verify(publicApi);
         }
     }
 }
