91b07fb80a1be750dc5e1e99ff2d039abbcd7391
Sean Feldman <feldman.sean@gmail.com>
2020-12-09 15:46:46 -0700

    Fix API Approvals

--- src/Tests/APIApprovals.cs
+++ src/Tests/APIApprovals.cs
@@ -9,7 +9,7 @@ public class APIApprovals
     [Test]
     public void Approve()
     {
-        var publicApi = typeof(EncryptedString).Assembly.GeneratePublicApi(new ApiGeneratorOptions { ExcludeAttributes = new[] { "System.Runtime.Versioning.TargetFrameworkAttribute" } });
+        var publicApi = typeof(EncryptedString).Assembly.GeneratePublicApi(new ApiGeneratorOptions { ExcludeAttributes = new[] { "System.Runtime.Versioning.TargetFrameworkAttribute", "System.Reflection.AssemblyMetadataAttribute" } });
         Approver.Verify(publicApi);
     }
 }
