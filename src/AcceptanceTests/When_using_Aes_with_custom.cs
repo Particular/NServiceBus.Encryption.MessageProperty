82089aca4439ee694516fd5d4464b91b1b26bfc8
Brandon Ording <bording@gmail.com>
2023-09-15 13:15:15 -0400

    Use C# 12 collection expressions (#454)

--- src/AcceptanceTests/When_using_Aes_with_custom.cs
+++ src/AcceptanceTests/When_using_Aes_with_custom.cs
@@ -84,11 +84,11 @@
 
                     testContext.SubPropertySecret = message.SubProperty.Secret.Value;
 
-                    testContext.CreditCards = new List<string>
-                    {
+                    testContext.CreditCards =
+                    [
                         message.CreditCards[0].Number.Value,
                         message.CreditCards[1].Number.Value
-                    };
+                    ];
 
                     testContext.GetTheMessage = true;
 
