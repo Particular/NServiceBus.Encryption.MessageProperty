f8e64e42a1d55a0d2ba5078372cda061ccdcc2a0
Andreas Öhlund <andreas.ohlund@particular.net>
2026-04-08 07:56:35 +0200

    Restore properties after encryption to support reuse

---
+++ src/AcceptanceTests/When_message_or_sub_properties_are_reused_unobtrusive.cs
@@ -0,0 +1,72 @@
+﻿namespace NServiceBus.Encryption.MessageProperty.AcceptanceTests;
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AcceptanceTesting;
+using NUnit.Framework;
+
+public class When_message_or_sub_properties_are_reused_unobtrusive : NServiceBusAcceptanceTest
+{
+    [Test]
+    public async Task Should_not_double_encrypt_properties()
+    {
+        var context = await Scenario.Define<Context>()
+            .WithEndpoint<Endpoint>(b => b.When(async session =>
+            {
+                var messageToReuse = new MessageWithSecretData
+                {
+                    Secret = "betcha can't guess my secret",
+                    SubProperty = new MySecretSubProperty { Secret = "My sub secret" }
+                };
+                await session.SendLocal(messageToReuse);
+                await session.SendLocal(messageToReuse);
+            }))
+            .Run();
+
+        Assert.Multiple(() =>
+        {
+            foreach (var messageToReuse in context.MessagesReceived)
+            {
+                Assert.That(messageToReuse.Secret, Is.EqualTo("betcha can't guess my secret"));
+                Assert.That(messageToReuse.SubProperty.Secret, Is.EqualTo("My sub secret"));
+            }
+        });
+    }
+
+    public class Context : ScenarioContext
+    {
+        public List<MessageWithSecretData> MessagesReceived { get; } = [];
+    }
+
+    public class Endpoint : EndpointConfigurationBuilder
+    {
+        public Endpoint()
+        {
+            var keys = new Dictionary<string, byte[]> { { "1st", "gdDbqRpqdRbTs3mhdZh9qCaDaxJXl+e6"u8.ToArray() } };
+
+            EndpointSetup<DefaultServer>(config => config.EnableMessagePropertyEncryption(new AesEncryptionService("1st", keys), t => t.Name.StartsWith("Secret")));
+        }
+
+        public class Handler(Context testContext) : IHandleMessages<MessageWithSecretData>
+        {
+            public Task Handle(MessageWithSecretData message, IMessageHandlerContext context)
+            {
+                testContext.MessagesReceived.Add(message);
+                testContext.MarkAsCompleted(testContext.MessagesReceived.Count == 2);
+
+                return Task.FromResult(0);
+            }
+        }
+    }
+
+    public class MessageWithSecretData : IMessage
+    {
+        public string Secret { get; set; }
+        public MySecretSubProperty SubProperty { get; set; }
+    }
+
+    public class MySecretSubProperty
+    {
+        public string Secret { get; set; }
+    }
+}
--- src/MessageProperty/EncryptBehavior.cs
+++ src/MessageProperty/EncryptBehavior.cs
@@ -1,63 +1,76 @@
 ﻿namespace NServiceBus.Encryption.MessageProperty
 {
     using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using System.Reflection;
     using System.Threading.Tasks;
     using Pipeline;
 
-    class EncryptBehavior : IBehavior<IOutgoingLogicalMessageContext, IOutgoingLogicalMessageContext>
+    class EncryptBehavior(EncryptionInspector messageInspector, IEncryptionService encryptionService) : IBehavior<IOutgoingLogicalMessageContext, IOutgoingLogicalMessageContext>
     {
-        public EncryptBehavior(EncryptionInspector messageInspector, IEncryptionService encryptionService)
-        {
-            this.messageInspector = messageInspector;
-            this.encryptionService = encryptionService;
-        }
-
-        public Task Invoke(IOutgoingLogicalMessageContext context, Func<IOutgoingLogicalMessageContext, Task> next)
+        public async Task Invoke(IOutgoingLogicalMessageContext context, Func<IOutgoingLogicalMessageContext, Task> next)
         {
             var currentMessageToSend = context.Message.Instance;
 
-            foreach (var item in messageInspector.ScanObject(currentMessageToSend))
+            var propertiesToRestore = new List<Tuple<object, Tuple<object, MemberInfo>>>();
+            var propertiesToEncrypt = messageInspector.ScanObject(currentMessageToSend);
+
+            try
             {
-                EncryptMember(item.Item1, item.Item2, context);
+                foreach (var item in propertiesToEncrypt)
+                {
+                    var oldValue = EncryptMember(item.Item1, item.Item2, context);
+
+                    propertiesToRestore.Add(new Tuple<object, Tuple<object, MemberInfo>>(oldValue, item));
+                }
+
+                context.UpdateMessage(currentMessageToSend);
+
+                await next(context).ConfigureAwait(false);
             }
+            finally
+            {
+                if (propertiesToEncrypt.Any())
+                {
+                    foreach (var propertyToRestore in propertiesToRestore)
+                    {
+                        propertyToRestore.Item2.Item2.SetValue(propertyToRestore.Item2.Item1, propertyToRestore.Item1);
+                    }
 
-            context.UpdateMessage(currentMessageToSend);
-
-            return next(context);
+                    context.UpdateMessage(currentMessageToSend);
+                }
+            }
         }
 
-        void EncryptMember(object message, MemberInfo member, IOutgoingLogicalMessageContext context)
+        object EncryptMember(object message, MemberInfo member, IOutgoingLogicalMessageContext context)
         {
             var valueToEncrypt = member.GetValue(message);
 
             if (valueToEncrypt is EncryptedString wireEncryptedString)
             {
+                var copy = new EncryptedString { Value = wireEncryptedString.Value };
                 encryptionService.EncryptValue(wireEncryptedString, context);
-                return;
+                return copy;
             }
 
             if (valueToEncrypt is string stringToEncrypt)
             {
+                var copy = stringToEncrypt;
                 encryptionService.EncryptValue(ref stringToEncrypt, context);
 
                 member.SetValue(message, stringToEncrypt);
-                return;
+                return copy;
             }
 
             throw new Exception("Only string properties are supported for convention based encryption. Check the configured conventions.");
         }
 
-        IEncryptionService encryptionService;
-        EncryptionInspector messageInspector;
-
         public class EncryptRegistration : RegisterStep
         {
             public EncryptRegistration(EncryptionInspector inspector, IEncryptionService encryptionService)
-                : base("MessagePropertyEncryption", typeof(EncryptBehavior), "Invokes the encryption logic", b => new EncryptBehavior(inspector, encryptionService))
-            {
+                : base("MessagePropertyEncryption", typeof(EncryptBehavior), "Invokes the encryption logic", b => new EncryptBehavior(inspector, encryptionService)) =>
                 InsertAfter("MutateOutgoingMessages");
-            }
         }
     }
 }
