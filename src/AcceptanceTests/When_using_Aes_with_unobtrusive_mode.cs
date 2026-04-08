79390b67dde6bfccfef3867c085923d3e355d95f
Andreas Öhlund <andreas.ohlund@particular.net>
2026-04-08 13:45:33 +0200

    Use existing tests instead

--- src/AcceptanceTests/When_using_Aes_with_unobtrusive_mode.cs
+++ src/AcceptanceTests/When_using_Aes_with_unobtrusive_mode.cs
@@ -1,133 +1,124 @@
-namespace NServiceBus.Encryption.MessageProperty.AcceptanceTests
+namespace NServiceBus.Encryption.MessageProperty.AcceptanceTests;
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AcceptanceTesting;
+using AcceptanceTesting.Customization;
+using NUnit.Framework;
+
+public class When_using_Aes_with_unobtrusive_mode : NServiceBusAcceptanceTest
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
-    using AcceptanceTesting;
-    using AcceptanceTesting.Customization;
-    using NUnit.Framework;
-
-    public class When_using_Aes_with_unobtrusive_mode : NServiceBusAcceptanceTest
+    [Test]
+    public async Task Should_receive_decrypted_message()
     {
-        [Test]
-        public async Task Should_receive_decrypted_message()
+        var messageToSend = new MessageWithSecretData
         {
-            var context = await Scenario.Define<Context>()
-                .WithEndpoint<Sender>(b => b.When(session => session.Send(new MessageWithSecretData
+            EncryptedSecret = "betcha can't guess my secret",
+            SubProperty = new MySecretSubProperty { EncryptedSecret = "My sub secret" },
+            CreditCards =
+            [
+                new CreditCardDetails
                 {
-                    EncryptedSecret = "betcha can't guess my secret",
-                    SubProperty = new MySecretSubProperty
-                    {
-                        EncryptedSecret = "My sub secret"
-                    },
-                    CreditCards =
-                    [
-                        new CreditCardDetails
-                        {
-                            ValidTo = DateTime.UtcNow.AddYears(1),
-                            EncryptedNumber = "312312312312312"
-                        },
-                        new CreditCardDetails
-                        {
-                            ValidTo = DateTime.UtcNow.AddYears(2),
-                            EncryptedNumber = "543645546546456"
-                        }
-                    ]
-                })))
-                .WithEndpoint<Receiver>()
-                .Done(c => c.GetTheMessage || c.FailedMessages.Any())
-                .Run();
+                    ValidTo = DateTime.UtcNow.AddYears(1),
+                    EncryptedNumber = "312312312312312"
+                },
+                new CreditCardDetails
+                {
+                    ValidTo = DateTime.UtcNow.AddYears(2),
+                    EncryptedNumber = "543645546546456"
+                }
+            ]
+        };
 
-            Assert.Multiple(() =>
-            {
-                Assert.That(context.Secret, Is.EqualTo("betcha can't guess my secret"));
-                Assert.That(context.SubPropertySecret, Is.EqualTo("My sub secret"));
-            });
+        var context = await Scenario.Define<Context>()
+            .WithEndpoint<Sender>(b => b.When(session => session.Send(messageToSend)))
+            .WithEndpoint<Receiver>()
+            .Run();
+
+        Assert.Multiple(() =>
+        {
+            Assert.That(context.Secret, Is.EqualTo(messageToSend.EncryptedSecret));
+            Assert.That(context.SubPropertySecret, Is.EqualTo(messageToSend.SubProperty.EncryptedSecret));
             Assert.That(context.CreditCards, Is.EquivalentTo(
             [
                 "312312312312312",
                 "543645546546456"
             ]));
-        }
+        });
+    }
 
-        static readonly Dictionary<string, byte[]> Keys = new()
-        {
-            {"1st", "gdDbqRpqdRbTs3mhdZh9qCaDaxJXl+e6"u8.ToArray()}
-        };
+    static readonly Dictionary<string, byte[]> Keys = new() { { "1st", "gdDbqRpqdRbTs3mhdZh9qCaDaxJXl+e6"u8.ToArray() } };
 
-        public class Context : ScenarioContext
-        {
-            public bool GetTheMessage { get; set; }
+    public class Context : ScenarioContext
+    {
+        public string Secret { get; set; }
 
-            public string Secret { get; set; }
+        public string SubPropertySecret { get; set; }
 
-            public string SubPropertySecret { get; set; }
+        public List<string> CreditCards { get; set; }
+    }
 
-            public List<string> CreditCards { get; set; }
-        }
-
-        public class Sender : EndpointConfigurationBuilder
-        {
-            public Sender() =>
-                EndpointSetup<DefaultServer>(c =>
-                    {
-                        c.Conventions().DefiningCommandsAs(t => t.Namespace != null && t.FullName == typeof(MessageWithSecretData).FullName);
-
-                        c.EnableMessagePropertyEncryption(new AesEncryptionService("1st", Keys), t => t.Name.StartsWith("Encrypted"));
-
-                        c.ConfigureRouting()
-                            .RouteToEndpoint(typeof(MessageWithSecretData), Conventions.EndpointNamingConvention(typeof(Receiver)));
-                    });
-        }
-
-        public class Receiver : EndpointConfigurationBuilder
-        {
-            public Receiver() =>
-                EndpointSetup<DefaultServer>(c =>
-                {
-                    c.Conventions().DefiningCommandsAs(t => t.Namespace != null && t.FullName == typeof(MessageWithSecretData).FullName);
-
-                    c.EnableMessagePropertyEncryption(new AesEncryptionService("1st", Keys), t => t.Name.StartsWith("Encrypted"));
-                });
-
-            public class Handler(Context testContext) : IHandleMessages<MessageWithSecretData>
+    public class Sender : EndpointConfigurationBuilder
+    {
+        public Sender() =>
+            EndpointSetup<DefaultServer>(c =>
             {
-                public Task Handle(MessageWithSecretData message, IMessageHandlerContext context)
-                {
-                    testContext.Secret = message.EncryptedSecret;
+                c.Conventions().DefiningCommandsAs(t => t.Namespace != null && t.FullName == typeof(MessageWithSecretData).FullName);
 
-                    testContext.SubPropertySecret = message.SubProperty.EncryptedSecret;
+                c.EnableMessagePropertyEncryption(new AesEncryptionService("1st", Keys), t => t.Name.StartsWith("Encrypted"));
 
-                    testContext.CreditCards =
-                    [
-                        message.CreditCards[0].EncryptedNumber,
-                        message.CreditCards[1].EncryptedNumber
-                    ];
+                c.ConfigureRouting()
+                    .RouteToEndpoint(typeof(MessageWithSecretData), Conventions.EndpointNamingConvention(typeof(Receiver)));
+            });
+    }
 
-                    testContext.GetTheMessage = true;
+    public class Receiver : EndpointConfigurationBuilder
+    {
+        public Receiver() =>
+            EndpointSetup<DefaultServer>(c =>
+            {
+                c.Conventions().DefiningCommandsAs(t => t.Namespace != null && t.FullName == typeof(MessageWithSecretData).FullName);
 
-                    return Task.CompletedTask;
-                }
+                c.EnableMessagePropertyEncryption(new AesEncryptionService("1st", Keys), t => t.Name.StartsWith("Encrypted"));
+            });
+
+        public class Handler(Context testContext) : IHandleMessages<MessageWithSecretData>
+        {
+            public Task Handle(MessageWithSecretData message, IMessageHandlerContext context)
+            {
+                testContext.Secret = message.EncryptedSecret;
+
+                testContext.SubPropertySecret = message.SubProperty.EncryptedSecret;
+
+                testContext.CreditCards =
+                [
+                    message.CreditCards[0].EncryptedNumber,
+                    message.CreditCards[1].EncryptedNumber
+                ];
+
+                testContext.MarkAsCompleted();
+
+                return Task.CompletedTask;
             }
         }
+    }
 
-        public class MessageWithSecretData
-        {
-            public string EncryptedSecret { get; set; }
-            public MySecretSubProperty SubProperty { get; set; }
-            public List<CreditCardDetails> CreditCards { get; set; }
-        }
+    public class MessageWithSecretData
+    {
+        public string EncryptedSecret { get; set; }
+        public MySecretSubProperty SubProperty { get; set; }
+        public List<CreditCardDetails> CreditCards { get; set; }
+    }
 
-        public class CreditCardDetails
-        {
-            public DateTime ValidTo { get; set; }
-            public string EncryptedNumber { get; set; }
-        }
+    public class CreditCardDetails
+    {
+        public DateTime ValidTo { get; set; }
+        public string EncryptedNumber { get; set; }
+    }
 
-        public class MySecretSubProperty
-        {
-            public string EncryptedSecret { get; set; }
-        }
+    public class MySecretSubProperty
+    {
+        public string EncryptedSecret { get; set; }
     }
 }
